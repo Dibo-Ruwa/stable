27693bda5aef870699378e2271f5710e5372b71c
muhammod1 <muhammodholawale@gmail.com>
2024-11-27 08:53:20 +0100

    Feat: AboutUs page > Header section

---
+++ containers/about-us/AboutUs.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AboutUs.css'
+import { AboutHeader } from './components/AboutHeader'
+export const AboutUs = () => {
+  return (
+    <div className='AboutUs_Container'>
+      <AboutHeader />
+    </div>
+  )
+}
