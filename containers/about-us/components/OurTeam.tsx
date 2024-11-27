7d7a3b9edfe5e48182f301e741d75debea5b46bf
muhammod1 <muhammodholawale@gmail.com>
2024-11-27 20:44:40 +0100

    Feat: Aboutus page > OurTeam section

--- containers/about-us/components/OurTeam.tsx
+++ containers/about-us/components/OurTeam.tsx
@@ -20,7 +20,7 @@ export const OurTeam = () => {
                 alt="Team image"
                 width={314}
                 height={396}
-                className="OurTeam_Card_img"
+                className="OurTeam_Card_img Green_image"
               />
             </div>
             <div className="Teams_Name_skill">
@@ -32,11 +32,11 @@ export const OurTeam = () => {
           <div className="OurTeam_Card Blue_Card">
             <div className="OurTeam_CardImage">
               <Image
-                src="/images/image 22.png"
+                src="/images/image 23.png"
                 alt="Team image"
                 width={314}
                 height={396}
-                className="OurTeam_Card_img"
+                className="OurTeam_Card_img blue_image"
               />
             </div>
             <div className="Teams_Name_skill">
@@ -45,7 +45,40 @@ export const OurTeam = () => {
             </div>
           </div>
         </div>
-        <div></div>
+
+        <div className="OurTeams_Cards">
+          <div className="OurTeam_Card Purple_Card">
+            <div className="OurTeam_CardImage">
+              <Image
+                src="/images/image 24.png"
+                alt="Team image"
+                width={314}
+                height={396}
+                className="OurTeam_Card_img Purple_image"
+              />
+            </div>
+            <div className="Teams_Name_skill">
+              <p className="Teams_Name">Mustapha Idris</p>
+              <p className="Teams_Skills"> Marketing & Customer Success</p>
+            </div>
+          </div>
+
+          <div className="OurTeam_Card periwinkleBlue_Card">
+            <div className="OurTeam_CardImage">
+              <Image
+                src="/images/image 25.png"
+                alt="Team image"
+                width={314}
+                height={396}
+                className="OurTeam_Card_img periwinkleBlue_image"
+              />
+            </div>
+            <div className="Teams_Name_skill">
+              <p className="Teams_Name">Mustapha Idris</p>
+              <p className="Teams_Skills"> Marketing & Customer Success</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
