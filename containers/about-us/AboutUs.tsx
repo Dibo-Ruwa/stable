a8a99961022e936c8badd8e816b7f43a00f75038
muhammod1 <muhammodholawale@gmail.com>
2024-11-28 08:05:58 +0100

    Feat: Aboutus page > Join Us section

--- containers/about-us/AboutUs.tsx
+++ containers/about-us/AboutUs.tsx
@@ -3,12 +3,14 @@ import './AboutUs.css'
 import { AboutHeader } from './components/AboutHeader'
 import { WSA } from './components/WSA'
 import { OurTeam } from './components/OurTeam'
+import { JoinUs } from './components/JoinUs'
 export const AboutUs = () => {
   return (
     <div className='AboutUs_Container'>
       <AboutHeader />
       <WSA />
       <OurTeam />
+      <JoinUs />
     </div>
   )
 }
