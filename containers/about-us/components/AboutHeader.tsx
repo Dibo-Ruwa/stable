c6bd317f80f56ba05ef76acd7f876d50c56fc7d3
muhammod1 <muhammodholawale@gmail.com>
2025-01-20 20:18:30 +0100

    Change services in about header dynamically and animatedly

--- containers/about-us/components/AboutHeader.tsx
+++ containers/about-us/components/AboutHeader.tsx
@@ -1,8 +1,53 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
-import '../AboutUs.css'
+import "../AboutUs.css";
+
+interface serviceChange {
+  image: string;
+  title: string;
+}
+
+const Services: serviceChange[] = [
+  {
+    image: "/images/image 15.png",
+    title: "Moving",
+  },
+  {
+    image: "/images/image 15.png",
+    title: "Food",
+  },
+  {
+    image: "/images/image 15.png",
+    title: "Groceries",
+  },
+  {
+    image: "/images/image 15.png",
+    title: "Laundry",
+  },
+  {
+    image: "/images/image 15.png",
+    title: "Cleaning",
+  },
+];
+
 export const AboutHeader = () => {
+  const [currentServiceIndex, setCurrentServiceIndex] = useState(0);
+
+  // Cycle through services every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentServiceIndex((prevIndex) =>
+        prevIndex === Services.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Change every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup interval
+  }, []);
+
+  const currentService = Services[currentServiceIndex];
+
   return (
     <section className="AboutHeader_Container">
       <div className="Header_LeftContainer">
@@ -18,16 +63,25 @@ export const AboutHeader = () => {
         <div className="AboutHeader_content">
           <p className="AboutHeader_Title">
             DIBORUWA is your go-to solution for{" "}
-            <span className="AboutHeader_ChangingText">
-              Moving{" "}
-              <Image
-                src="/images/image 15.png"
-                alt="moving"
-                width={24}
-                height={24}
-                className="AboutHeader_ChangingImage"
-              />
-            </span>
+            <AnimatePresence mode="wait">
+              <motion.span
+                key={currentService.title} // Unique key for animation
+                initial={{ y: 30, opacity: 0 }} // Start off-screen below
+                animate={{ y: 0, opacity: 1 }} // Slide up into view
+                exit={{ y: -30, opacity: 0 }} // Slide out upwards
+                transition={{ duration: 0.5 }} // Smooth transition
+                className="AboutHeader_ChangingText"
+              >
+                {currentService.title}{" "}
+                <Image
+                  src={currentService.image}
+                  alt={currentService.title}
+                  width={24}
+                  height={24}
+                  className="AboutHeader_ChangingImage"
+                />
+              </motion.span>
+            </AnimatePresence>
           </p>
           <p className="AboutHeader_Description">
             we've been committed to simplifying your life by offering a range of
